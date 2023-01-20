6b191d279763cb1b562e722be3f710f560cef38e
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-01-20 11:18:52 -0300

    fix: host name

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import { app } from './app'
 
+const HOST = process.env.HOST
+
 async function start(): Promise<void> {
   await app.listen({
+    host: HOST,
     port: 3333
   })
   console.log('Server is running')
